a8909efd1e5785e9e0ae5297e6823888533de7a0
Sjoerd <sbeetsma99@gmail.com>
2021-05-17 20:06:33 +0200

    Add test to tests FSM run and equals with weights instead of characters

--- test/com/FSMTest.java
+++ test/com/FSMTest.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FSMTest {
-
     @Test
+    // test run met een tekstinvoer van characters
     void run() {
         // nodes maken
         Node n1 = new Node("n1");
@@ -28,6 +28,32 @@ class FSMTest {
         // alleen de eerste overgang A bestaat. Dus padlengte is 4 + 1
         assertEquals(5, fsm1.getPadLengte());
     }
+    // test run met overgangen op gewichten
+    @Test
+    void run_gewichten() {
+        // nodes maken
+        Node n1 = new Node("n1");
+        Node n2 = new Node("n2");
+        Node n3 = new Node("n3");
+        // overgangen
+        n1.addGewichtOvergang(n2,1);
+        n1.addGewichtOvergang(n3,1);
+
+        n2.addGewichtOvergang(n1,1);
+        n2.addGewichtOvergang(n3,1);
+
+        n3.addGewichtOvergang(n1,1);
+        n3.addGewichtOvergang(n2,1);
+
+        FSM fsm1 = new FSM(n1);
+        fsm1.run("10");
+        // start node + aantal stappen (tekstinvoer naar int) zal 11 lang moeten zijn
+        assertEquals(11, fsm1.getPadLengte());
+        // run extra stappen vanaf de huidige nodge
+        fsm1.run("4");
+        // padlengte is startnode 1 + 10stappen + 4stappen
+        assertEquals(15, fsm1.getPadLengte());
+    }
 
     @Test
     void equals() {
@@ -45,6 +71,27 @@ class FSMTest {
         // wel hetzelfde pad afgelopen
         fsm2.run("A");
         assertEquals(fsm1, fsm2);
+    }
+    // als er maar 1 mogelijk pad is met de bestaande gewichten moeten 2 fsm's hetzelfde zijn na evenveel stappen
+    @Test
+    void equals_gewichten() {
+        // nodes maken
+        Node n1 = new Node("n1");
+        Node n2 = new Node("n2");
+        Node n3 = new Node("n3");
+        // overgangen
+        n1.addGewichtOvergang(n2,1);
+        n2.addGewichtOvergang(n3,1);
+        n3.addGewichtOvergang(n1,1);
 
+        FSM fsm1 = new FSM(n1);
+        FSM fsm2 = new FSM(n1);
+
+        // niet hetzelfde pad afgelopen
+        fsm1.run("99");
+        assertNotEquals(fsm1, fsm2);
+        // wel hetzelfde pad afgelopen
+        fsm2.run("99");
+        assertEquals(fsm1, fsm2);
     }
 }
