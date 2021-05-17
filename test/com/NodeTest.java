7d9e6f1c08b65b42182fcd5416735887ed21caa5
Sjoerd <sbeetsma99@gmail.com>
2021-05-17 20:05:05 +0200

    Add test addGewichtovergang from class Node

--- test/com/NodeTest.java
+++ test/com/NodeTest.java
@@ -5,17 +5,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class NodeTest {
-    // Test overgangen toevoegen
+    // Test overgang toevoegen met character
     @Test
     void addOvergang() {
         Node n1 = new Node ("n1");
         Node n2 = new Node ("n2");
-        assertEquals(0, n1.getAantalOvergangen());
+        assertEquals(0, n1.getOvergangen().size());
         n1.addOvergang('A', n2);
-        assertEquals(1, n1.getAantalOvergangen());
+        assertEquals(1, n1.getOvergangen().size());
         n1.addOvergang('B', n1);
-        assertEquals(2, n1.getAantalOvergangen());
+        assertEquals(2, n1.getOvergangen().size());
     }
+    // Test overgang toevoegen met alleen gewicht
+    @Test
+    void addGewichtOvergang() {
+        Node n1 = new Node ("n1");
+        Node n2 = new Node ("n2");
+        assertEquals(0, n1.getOvergangen().size());
+        n1.addGewichtOvergang(n2,1);
+        assertEquals(1, n1.getGewichtOvergangen().size());
+        n1.addGewichtOvergang(n1,1);
+        assertEquals(2, n1.getGewichtOvergangen().size());
+    }
+
     // Test ophalen van overgangen
     @Test
     void getOvergang() {
